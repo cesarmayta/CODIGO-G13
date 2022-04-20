ba81da85978c354f9bbf0e824125d5e3f340f2ab
cesar mayta <cesarmayta@gmail.com>
2022-04-19 23:06:58 -0500

    dia2 firebase

--- semana02/dia2/main.py
+++ semana02/dia2/main.py
@@ -1,7 +1,18 @@
+from pydoc import doc
 from flask import Flask,render_template,request
 import requests
 
 URL = 'https://api.github.com/users/cesarmayta'
+
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("tokenfirebase.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 
 app = Flask(__name__)
 
@@ -35,7 +46,22 @@ def about():
 
 @app.route('/portafolio')
 def portafolio():
-    return render_template('portafolio.html')
+    colProyectos = db.collection('proyectos')
+    docProyectos = colProyectos.get()
+
+    #print(docProyectos)
+
+    lstProyectos = []
+    for doc in docProyectos:
+        print(doc.to_dict())
+        dicProyecto = doc.to_dict()
+        lstProyectos.append(dicProyecto)
+
+    context = {
+        'proyectos':lstProyectos
+    }
+
+    return render_template('portafolio.html',**context)
 
 @app.route('/contacto')
 def contacto():
