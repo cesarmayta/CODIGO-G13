89ecd531299b2c5a03d244965433de9be943d850
cesar mayta <cesarmayta@gmail.com>
2022-04-22 22:23:14 -0500

    dia5 auth firebase

--- semana02/dia5/app/admin/views.py
+++ semana02/dia5/app/admin/views.py
@@ -5,14 +5,35 @@ from . import admin
 #importamos los formularios
 from app.forms import LoginForm
 
+
+#### para autenticación de usuarios #######
+import pyrebase
+from app.auth_token import firebaseConfig
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+###########################################
+
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
 
-@admin.route('/login')
+@admin.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
     context = {
         'login_form':login_form
     }
+    #CODIGO PARA LOGIN DE USUARIOS
+    if login_form.validate_on_submit():
+        usuarioData = login_form.usuario.data
+        passwordData = login_form.password.data
+
+        try:
+            usuario = auth.sign_in_with_email_and_password(usuarioData,passwordData)
+            dataUsuarioValido = auth.get_account_info(usuario['idToken'])
+            print(dataUsuarioValido)
+        except:
+            print("usuario incorrecto")
+
     return render_template('admin/login.html',**context)
