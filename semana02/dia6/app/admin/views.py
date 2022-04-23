54f97908c3b6e07f21472fd50b74b1ffce91e65d
cesar mayta <cesarmayta@gmail.com>
2022-04-23 09:41:47 -0500

    dia6

---
+++ semana02/dia6/app/admin/views.py
@@ -0,0 +1,53 @@
+from flask import (
+    render_template,redirect,
+    url_for,session,
+    flash)
+
+from . import admin
+
+#importamos los formularios
+from app.forms import LoginForm
+
+
+#### para autenticación de usuarios #######
+import pyrebase
+from app.auth_token import firebaseConfig
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+###########################################
+
+@admin.route('/')
+def index():
+    if('token' in session):
+        return render_template('admin/index.html')
+    else:
+        return redirect(url_for('admin.login'))
+        
+@admin.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    context = {
+        'login_form':login_form
+    }
+    #CODIGO PARA LOGIN DE USUARIOS
+    if login_form.validate_on_submit():
+        usuarioData = login_form.usuario.data
+        passwordData = login_form.password.data
+
+        try:
+            usuario = auth.sign_in_with_email_and_password(usuarioData,passwordData)
+            dataUsuarioValido = auth.get_account_info(usuario['idToken'])
+            print(dataUsuarioValido)
+            session['token'] = usuario['idToken']
+            return redirect(url_for('admin.index'))
+        except:
+            print("usuario incorrecto")
+            flash("Usuario o password invalidos")
+
+    return render_template('admin/login.html',**context)
+
+@admin.route('/logout')
+def logout():
+    session.pop('token')
+    return redirect(url_for('admin.login'))
