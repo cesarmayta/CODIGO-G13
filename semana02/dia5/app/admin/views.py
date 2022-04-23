2036c88cdf0fb4b5949b7cdba98a97eb87a34956
cesar mayta <cesarmayta@gmail.com>
2022-04-22 21:33:15 -0500

    dia5 wtf

--- semana02/dia5/app/admin/views.py
+++ semana02/dia5/app/admin/views.py
@@ -2,10 +2,17 @@ from flask import render_template
 
 from . import admin
 
+#importamos los formularios
+from app.forms import LoginForm
+
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
 
 @admin.route('/login')
 def login():
-    return render_template('admin/login.html')
+    login_form = LoginForm()
+    context = {
+        'login_form':login_form
+    }
+    return render_template('admin/login.html',**context)
