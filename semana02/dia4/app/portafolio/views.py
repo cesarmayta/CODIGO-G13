1ec3ffdcc183d2299b79c1ffe8af25095751daf7
cesar mayta <cesarmayta@gmail.com>
2022-04-22 20:00:20 -0500

    dia4 app factory portafolio listo

--- semana02/dia4/app/portafolio/views.py
+++ semana02/dia4/app/portafolio/views.py
@@ -36,3 +36,30 @@ def home():
 
     session['enlaces'] = lstEnlaces
     return render_template('portafolio/home.html')
+
+############### RUTAS DE MIS PAGINAS
+@portafolio.route('/acercade')
+def about():
+    return render_template('portafolio/acercade.html')
+
+@portafolio.route('/proyectos')
+def proyectos():
+    colProyectos = db.collection('proyectos')
+    docProyectos = colProyectos.get()
+
+    lstProyectos = []
+    for doc in docProyectos:
+        print(doc.to_dict())
+        dicProyecto = doc.to_dict()
+        lstProyectos.append(dicProyecto)
+
+
+    context = {
+        'proyectos':lstProyectos
+    }
+    return render_template('portafolio/portafolio.html',**context)
+
+@portafolio.route('/contacto')
+def contacto():
+    return render_template('portafolio/contacto.html')
+
