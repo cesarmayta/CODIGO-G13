55010500a8c99563e0a515f377c5b949e0f2abde
cesar mayta <cesarmayta@gmail.com>
2022-04-29 20:01:43 -0500

    dia4 parte 2

--- semana03/dia4/procedimientos_almacenados.sql
+++ semana03/dia4/procedimientos_almacenados.sql
@@ -1,11 +1,14 @@
 DELIMITER $$
+DROP PROCEDURE IF EXISTS sp_matricular_alumno;
 CREATE PROCEDURE sp_matricular_alumno(IN alu_id INT,IN niv_id INT,IN mod_id INT)
 BEGIN
     --variables
     DECLARE matId INT;
     DECLARE curId INT;
+    DECLARE totalCursos INT;
     set matId = 0;
     set curId = 1;
+    set totalCursos = 0;
 
     --insertar datos en la tabla matricula
     insert into tbl_matricula(alumno_id,nivel_id,modulo_id)
@@ -13,7 +16,9 @@ BEGIN
 
     select max(matricula_id) into matId from tbl_matricula;
 
-    WHILE curId <= 6 DO
+    select count(*) into totalCursos from tbl_curso;
+
+    WHILE curId <= totalCursos DO
         insert into tbl_matricula_curso(matricula_id,curso_id)
         values (matId,curId);
 
@@ -25,7 +30,7 @@ $$
 
 DELIMITER ;
 
-CALL sp_matricular_alumno(4,1,1);
+CALL sp_matricular_alumno(5,1,1);
 
 select * from tbl_matricula;
 
