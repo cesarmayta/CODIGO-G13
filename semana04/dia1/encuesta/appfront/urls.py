c359e91f6b59099967c12f9b17c44fd9c66da710
cesar mayta <cesarmayta@gmail.com>
2022-05-02 23:06:13 -0500

    dia1 parte  3

--- semana04/dia1/encuesta/appfront/urls.py
+++ semana04/dia1/encuesta/appfront/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'appfront'
+
 urlpatterns = [
     path('',views.app_index,name='index'),
-    path('<int:pregunta_id>',views.detalle,name='detalle')
+    path('<int:pregunta_id>',views.detalle,name='detalle'),
+    path('enviar',views.enviar,name='enviar')
 ]
