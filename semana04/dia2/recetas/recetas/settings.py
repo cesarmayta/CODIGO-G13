fd0bdad04fe2cb33ebabc1a9a2792fe2d0d2885b
cesar mayta <cesarmayta@gmail.com>
2022-05-03 22:10:18 -0500

    dia2 crispy

--- semana04/dia2/recetas/recetas/settings.py
+++ semana04/dia2/recetas/recetas/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
