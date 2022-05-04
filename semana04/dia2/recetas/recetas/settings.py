4d09da413098658123bcf1e5b0d95cab841dc04a
cesar mayta <cesarmayta@gmail.com>
2022-05-03 22:58:34 -0500

    dia2 mysql

--- semana04/dia2/recetas/recetas/settings.py
+++ semana04/dia2/recetas/recetas/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'recetas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_receta',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
