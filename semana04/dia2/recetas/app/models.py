3977392e597d5f6855174694a95e3e232887d411
cesar mayta <cesarmayta@gmail.com>
2022-05-03 20:27:46 -0500

    dia2 mtv

--- semana04/dia2/recetas/app/models.py
+++ semana04/dia2/recetas/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Receta(models.Model):
+    titulo = models.CharField(max_length=100)
+    ingredientes = models.TextField(help_text='redacta los ingredientes')
+    preparacion = models.TextField()
+    autor = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.titulo
--- semana04/dia2/recetas/app/views.py
+++ semana04/dia2/recetas/app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Receta
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    listaRecetas = Receta.objects.all()
+    print(listaRecetas)
+    context = {
+        'recetas':listaRecetas
+    }
+    return render(request,'index.html',context)
