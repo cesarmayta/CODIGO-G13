28c14fa820d3992ff00480cbca3dfab1e8d9192e
cesar mayta <cesarmayta@gmail.com>
2022-05-05 22:57:55 -0500

    dia4 catalogo

--- semana04/dia4/web/views.py
+++ semana04/dia4/web/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Categoria,Producto
+
 # Create your views here.
+def index(request):
+    listaProductos = Producto.objects.all()
+    context = {
+        'productos':listaProductos
+    }
+    return render(request,'index.html',context)
