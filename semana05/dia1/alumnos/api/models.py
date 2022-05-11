48ca93f2baff38a2c683570200724c7fbb0fffb2
cesar mayta <cesarmayta@gmail.com>
2022-05-10 20:31:28 -0500

    dia1 api get

--- semana05/dia1/alumnos/api/models.py
+++ semana05/dia1/alumnos/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    
--- semana05/dia1/alumnos/api/views.py
+++ semana05/dia1/alumnos/api/views.py
@@ -1,6 +1,25 @@
 from django.http import JsonResponse
 
+from .models import Alumno
+
 def index(request):
     return JsonResponse({
         'mensaje':'Bienvenido a mi Api'
     })
+
+def alumnos(request):
+    dataAlumnos = Alumno.objects.all()
+    lstAlumnos = []
+    for alumno in dataAlumnos:
+        dicAlumno = {
+            'nombre': alumno.nombre,
+            'email':alumno.email
+        }
+        lstAlumnos.append(dicAlumno)
+
+    context = {
+        'status':'ok',
+        'data': lstAlumnos
+    }
+    
+    return JsonResponse(context)
