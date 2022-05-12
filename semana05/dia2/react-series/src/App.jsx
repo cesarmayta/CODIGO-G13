3201b2ba9f6b9575fbf1ef9fa85bc87373952007
cesar mayta <cesarmayta@gmail.com>
2022-05-11 22:55:16 -0500

    dia2 stack inicial front back

--- semana05/dia2/react-series/src/App.jsx
+++ semana05/dia2/react-series/src/App.jsx
@@ -1,5 +1,8 @@
 import React,{Component} from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
 
 class App extends Component{
   constructor(props){
@@ -20,12 +23,43 @@ class App extends Component{
     axios.get('http://localhost:8000/api/series/')
     .then(res =>{
       console.log(res.data)
+      this.setState({series: res.data})
     })
   }
 
   render(){
     return(
-      <div>Series</div>
+      <div>
+        <Container>
+        <h1>Series</h1>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nombre</th>
+              <th>Fecha</th>
+              <th>Rating</th>
+              <th>Categoria</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.series.map((serie,index)=>{
+              return (
+                <tr key={serie.id}>
+                  <td>{serie.id}</td>
+                  <td>{serie.name}</td>
+                  <td>{serie.release_date}</td>
+                  <td>{serie.rating}</td>
+                  <td>{serie.category}</td>
+                  <td></td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
+        </Container>
+      </div>
     )
   }
 }
