236fcffd3ccb13e9ec5b70c59b5dbdfa1a557674
cesar mayta <cesarmayta@gmail.com>
2022-05-17 20:10:02 -0500

    cambios en api rest en fron con react

--- semana05/dia5/pos_frontend/src/environments/environments.js
+++ semana05/dia5/pos_frontend/src/environments/environments.js
@@ -1,3 +1,4 @@
-export const URL_BACKEND = 'http://127.0.0.1:8000'
+export const URL_BACKEND = 'https://cmaytaposbackend.herokuapp.com'
+//'http://127.0.0.1:8000'
 //'https://backposapi.herokuapp.com'
 	
