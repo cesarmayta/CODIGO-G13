a34608a67ee80adb0f3d77b2c62a46334c633c2b
cesar mayta <cesarmayta@gmail.com>
2022-06-02 19:48:59 -0500

    dia4

---
+++ semana08/dia4/middlewares/index.js
@@ -0,0 +1,40 @@
+const express  = require('express');
+
+const app = express();
+
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
+});
+
+app.get("/",(req,res)=>{
+    res.json({
+        content:"ejemplo de middlewares"
+    })
+})
+
+//middleware para una ruta
+
+app.use('/usuario',function(req,res,next){
+    console.log(a + 3);
+    console.log('tipo request : ',req.method);
+    next();
+})
+
+app.get("/usuario",(req,res)=>{
+    res.json({
+        nombre:'cesar'
+    })
+})
+
+//MIDDLEWARE DE ERRORES
+app.use(function(err,req,res,next){
+    console.error(err.stack);
+    res.status(500).json({
+        status:false,
+        content:'error en el servidor',
+        detail:err.stack
+    })
+})
+
+app.listen(5000,()=>console.log("http://localhost:5000"));
