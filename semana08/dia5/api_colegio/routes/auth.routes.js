82f760549af6d1caf57b109ec923e79d8da03fbb
cesar mayta <cesarmayta@gmail.com>
2022-06-03 23:14:04 -0500

    dia5 login completo

--- semana08/dia5/api_colegio/routes/auth.routes.js
+++ semana08/dia5/api_colegio/routes/auth.routes.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const UsuarioService = require('../services/usuario.service');
 
 function authApi(app){
     const router = express.Router();
     app.use("/auth",router);
 
-    router.post("/",async function(req,res){
-        const {usuario,password} = req.body;
+    objUsuarioService = new UsuarioService();
 
-        if(usuario === "admin" && password === "1234"){
+    router.post("/",async function(req,res){
+        const {body: usuario} = req;
+        console.log("paso 1 ) data enviado por post : \n " + usuario.usuario + " - " + usuario.password)
+        //llamamos el servicio de usuario para validar los datos
+        const dataValidate = await objUsuarioService.validate({usuario});
+        console.log("data validada : " + dataValidate.id)
+        if(dataValidate.id > 0){
             const token = jwt.sign(
-                {usuario:'admin'},
+                dataValidate,
                 'codigo2022',
                 {
                     expiresIn:'1h'
