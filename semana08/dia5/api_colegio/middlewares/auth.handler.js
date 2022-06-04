c86757e9e650a17ff1f560c7e6aaa5d9d02d87c8
cesar mayta <cesarmayta@gmail.com>
2022-06-03 20:54:01 -0500

    dia5 jwt

--- semana08/dia5/api_colegio/middlewares/auth.handler.js
+++ semana08/dia5/api_colegio/middlewares/auth.handler.js
@@ -1,10 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+
 function verifyToken(req,res,next){
-    const token = req.headers['api'];
-    if(token === "qwerty123"){
-        next();
-    }else{
-        res.status(401).json({
-            message:"no esta autorizado para ingresar"
+    const bearerToken = req.headers['authorization'];
+    console.log('bearer Token =',bearerToken)
+    if(typeof bearerToken !== 'undefined'){
+
+        //obtenemos el token enviado
+        const bearer = bearerToken.split(' ');
+        const token = bearer[1];
+
+        try{
+            const decoded = jwt.verify(token,'codigo2022');
+            console.log(decoded);
+        }catch(err){
+            return res.status(401).json({
+                status:false,
+                content:'token invalido'
+            })
+        }
+
+        return next();
+
+    }
+    else{
+        res.status(403).json({
+            status:false,
+            content:'no se encontro el token'
         })
     }
 }
