867e2c740ce1f2de908eb6db5a3a23a20376f433
cesar mayta <cesarmayta@gmail.com>
2022-06-08 20:41:57 -0500

    dia3 models

--- semana09/dia3/backend/api_django/shop/settings.py
+++ semana09/dia3/backend/api_django/shop/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_shop',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- semana09/dia3/backend/api_django/tienda/models.py
+++ semana09/dia3/backend/api_django/tienda/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from cloudinary.models import CloudinaryField
+
 # Create your models here.
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
@@ -7,3 +9,11 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
 
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    imagen = CloudinaryField('image',default='')
+    precio = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    categoria = models.ForeignKey(Categoria,on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.nombre
